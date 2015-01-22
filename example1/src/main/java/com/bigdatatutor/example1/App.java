bce9735f0ed9f7438cce6c18ad919557486891bc
bigdatatutor <bigdatatutor@192.168.1.163>
2015-01-21 20:48:44 -0500

    Test checkin

---
+++ example1/src/main/java/com/bigdatatutor/example1/App.java
@@ -0,0 +1,13 @@
+package com.bigdatatutor.example1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
